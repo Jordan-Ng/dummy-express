78631d2073dbc6616317f02c29a9152b372cc858
Jordan Ng <jordan930113@gmail.com>
2025-02-27 17:36:05 -0800

    reading hostname for dynamic identifier

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express')
+const fs = require("fs")
 const app = express()
 const port = 8000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  fs.readFile("/etc/hostname", "utf8", (err,data) => {
+    if (err) {
+      console.log("err", err)
+      res.send(500)
+      return
+    }
+    res.send(`Hello World from instance ${data.trim()}`)    
+  })
 })
 
 app.listen(port, () => {
