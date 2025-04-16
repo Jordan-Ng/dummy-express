344cbd1e4a9c4f84f0b60c54def3308eba961176
Jordan Ng <jordan930113@gmail.com>
2025-04-15 18:56:35 -0700

    adding NODE_APP_SERVICE environment variable for dynamic application config

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const fs = require("fs")
 const app = express()
 const port = 8000
 
-app.get('/', (req, res) => {
+app.get('/', (req, res) => {  
   fs.readFile("/etc/hostname", "utf8", (err,data) => {
     if (err) {
       console.log("err", err)
       res.send(500)
       return
     }
-    res.send(`Hello World from instance ${data.trim()}`)    
+    res.send(`Application: ${process.env.NODE_APP_SERVICE || "unknown"}, Instance ${data.trim()}`)    
   })
 })
 
